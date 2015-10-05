045f25ba6ac15cc2b1e2faf48b4a7fc1e40423de
Jake Causon <jake.causon@freshrelevance.com>
2015-10-05 10:29:56 +0100

    Add option for no email notifications

--- pyresponse.py
+++ pyresponse.py
@@ -10,7 +10,7 @@ from suds.client import Client as Suds
 import types
 
 class PureResponseClient(object):
-    version = '1.2.7'
+    version = '1.2.8'
 
     api_client = None
     api_account_level = None
